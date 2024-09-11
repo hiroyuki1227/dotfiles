c8c86c2a597ce00f2b2b71bacc3aa4b7605ded74
hrsuda1227 <hiroyuki.suda54@gmail.com>
2024-09-11 23:23:07 +0900

    joeson nvim development implimentation

---
+++ nvim/.config/nvim/lua/josean/core/init.lua
@@ -0,0 +1,4 @@
+require("josean.core.options")
+require("josean.core.keymaps")
+require("josean.core.snippets")
+require("josean.core.highlight")
--- nvim/.config/nvim/lua/user/plugins/tmux-navigator.lua
+++
@@ -1,6 +0,0 @@
-return {
-	{
-		"nvim-lua/plenary.nvim",
-		"christoomey/vim-tmux-navigator", -- tmux & split window navigation
-	},
-}
