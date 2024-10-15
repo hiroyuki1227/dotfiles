ff50345ae656640c6075640e9e18db5179faa7f5
hiroyuki1227 <hiroyuki.suda54@gmail.com>
2024-10-15 15:35:19 +0900

    neovim nvim-tree floating

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -17,6 +17,13 @@ return {
   },
 
   {
+    "nvim-tree/nvim-tree.lua",
+    cmd = "NvimTreeToggle",
+    config = function()
+      require("configs.nvim-tree")
+    end,
+  },
+  {
     "windwp/nvim-ts-autotag",
     ft = {
       "javascript",
