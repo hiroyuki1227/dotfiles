04e602543ecc063da0a68e10decadefe64571cef
hiroyuki1227 <hiroyuki.suda54@gmail.com>
2025-02-20 14:14:45 +0900

    modification neovim dashboard(snocks)

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -485,15 +485,48 @@ return {
       gitbrowse = { enabled = true },
       dashboard = {
         enabled = true,
-        lazy = false,
         sections = {
           { section = "header" },
-          { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          {
+            pane = 2,
+            section = "terminal",
+            cmd = "colorscript -e square",
+            -- cmd = "ascii-image-converter ~/.config/nvim/images/Profile.jpg -C -c",
+            height = 5,
+            padding = 1,
+          },
+          { section = "keys", gap = 1, padding = 1 },
+          { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          {
+            pane = 2,
+            icon = " ",
+            title = "Git Status",
+            section = "terminal",
+            enabled = function()
+              -- return vim.g.sna.git.get_root() ~= nil
+              return require("snacks").git.get_root() ~= nil
+            end,
+            cmd = "git status --short --branch --renames",
+            height = 5,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 3,
+          },
           { section = "startup" },
         },
       },
+      -- dashboard = {
+      --   enabled = true,
+      --   lazy = false,
+      --   sections = {
+      --     { section = "header" },
+      --     { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+      --     { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+      --     { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+      --     { section = "startup" },
+      --   },
+      -- },
     },
   },
 }
